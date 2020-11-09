679a2c2177dadbf8e559c637ddeda0c9535fe667
Ivan Mercedes <ivanmercede@gmail.com>
2020-11-09 21:37:36 +0100

    Adding mongoDB config and router

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+const conectarDB = require('./config/db');
 // crear el servivor
 const app = express();
 
-console.log('Comenzando Node Send');
+// Conectar a la base de datos
+conectarDB();
 
 // Puerto de la app
 const port = process.env. PORT || 4000;
+
+
+// Rutas de la app
+app.use('/api/usuarios', require('./routes/usuarios'))
 
 app.listen(port, '0.0.0.0', ()=>{
     console.log(`Servidor funcionando en el puerto ${port}`);
