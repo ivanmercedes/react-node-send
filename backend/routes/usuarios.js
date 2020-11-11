7869598781f16a5e533536e28fde0a33cdbdcb2b
Ivan Mercedes <ivanmercede@gmail.com>
2020-11-11 15:46:37 +0100

    Ending userController

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController')
+const { check } = require('express-validator');
 
 router.post('/',
+    [
+        check('nombre','El nombre es obligatorio').not().isEmpty(),
+        check('email','Agrega un email valido').isEmail(),
+        check('password','El password debe ser de al menos 6 caractares').isLength({min: 6})
+    ],
     usuarioController.nuevoUsuario
 );
 
