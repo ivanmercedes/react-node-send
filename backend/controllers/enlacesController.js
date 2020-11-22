4712d2d29ed3480c4c4acb539f3e7f6647029afe
Ivan Mercedes <ivanmercede@gmail.com>
2020-11-22 17:41:13 +0100

    Creating Schema and store enlaces to DB

---
+++ backend/controllers/enlacesController.js
@@ -0,0 +1,48 @@
+const Enlaces = require('../models/Enlace');
+const shortid = require('shortid');
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
+
+exports.nuevoEnlace = async (req, res, next) =>{
+    // revisar si hay errores
+     // mostrar mensajes de error de express validator
+     const errores = validationResult(req);
+     if(!errores.isEmpty()){
+         return res.status(400).json({ errores: errores.array() });
+     }
+
+    // Crear un objecto de Enlace
+    const { nombre_original } = req.body;
+
+    const enlace = new Enlaces();
+    enlace.url = shortid.generate();
+    enlace.nombre = shortid.generate();
+    enlace.nombre_original = nombre_original;
+    
+
+    // Si el usuario esta autenticado
+    if(req.usuario){
+        const { password, descargas } = req.body;
+        // asignar a enlace el numero de descargas
+        if(descargas){
+            enlace.descargas = descargas;
+        }
+        //asignar un password
+        if(password){
+            const salt = await bcrypt.genSalt(10);
+            enlace.password = await bcrypt.hash(password, salt);
+        }
+
+        //asignar el autor
+        enlace.autor = req.usuario.id;
+    }
+
+    // almacenar en la BD
+    try {
+        await enlace.save();
+        return res.json({msg: `${enlace.url}`});
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
