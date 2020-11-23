4bd5953e88c7c0f0bee90f0782ee6f4fdd3c5d97
Ivan Mercedes <ivanmercede@gmail.com>
2020-11-23 04:20:05 +0100

    Adding login and register pages with validation form

--- frontend/pages/crearcuenta.js
+++ frontend/pages/crearcuenta.js
@@ -4,33 +4,35 @@ import {  useFormik } from 'formik';
 import * as Yup from 'yup';
 
 
+const CrearCuenta = () => {
 
-const Login = () => {
+    //Formulario y validacion con formik y Yup
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            nombre: Yup.string()
+                        .required('El nombre es obligatorio'),
+            email: Yup.string()
+                        .email('El email no es valido')
+                        .required('El email es obligatorio'),
+            password: Yup.string()
+                        .required('El password no puede ir vacio')
+                        .min(6,'El password debe tener al menos 6 caracteres')
 
-   //Formulario y validacion con formik y Yup
-   const formik = useFormik({
-    initialValues: {
-        email: '',
-        password: ''
-    },
-    validationSchema: Yup.object({
-        email: Yup.string()
-                    .email('El email no es valido')
-                    .required('El email es obligatorio'),
-        password: Yup.string()
-                    .required('El password no puede ir vacio')
-
-    }),
-    onSubmit: valores =>{
-        console.log(valores);
-    }
-});
-
+        }),
+        onSubmit: valores =>{
+            console.log(valores);
+        }
+    });
 
   return ( 
     <Layout>
        <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
-            <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Iniciar Sesion</h2>
+            <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Crear Cuenta</h2>
             
             <div className="flex justify-center mt-5">
                 <div className="w-full max-w-lg">
@@ -38,8 +40,28 @@ const Login = () => {
                     className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
                     onSubmit={formik.handleSubmit}
                     >
-                 
-                         
+                        <div className="mb-4">
+                            <label
+                             htmlFor="nombre" 
+                             className="block text-black text-sm font-bold mb-2"
+                             >Nombre</label>
+                            <input 
+                            type="text"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="nombre"
+                            placeholder="Nombre de Usuario"
+                            value={formik.values.nombre}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            />
+
+                            { formik.touched.nombre && formik.errors.nombre  ? (
+                                <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                                    <p className="Font-bold">Error</p>
+                                    <p>{ formik.errors.nombre}</p>
+                                </div>
+                            ): null}
+                        </div>
 
                         <div className="mb-4">
                             <label
@@ -90,7 +112,7 @@ const Login = () => {
                         <input 
                             type="submit"
                             className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white font-bold uppercase"
-                            value="Iniciar Sesion"
+                            value="Crear Cuenta"
                         />
                     </form>
                 </div>
@@ -100,4 +122,4 @@ const Login = () => {
    );
 }
  
-export default Login;
+export default CrearCuenta;
