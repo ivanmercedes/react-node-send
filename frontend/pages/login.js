5bae1d48cc93cd1894ed0ddd2b50d38b6997bc7f
Ivan Mercedes <ivanmercede@gmail.com>
2020-11-23 03:14:32 +0100

    Configure NextJs project

---
+++ frontend/pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/Layout';
+
+
+const Login = () => {
+  return ( 
+    <Layout>
+      <h1>Login</h1>
+    </Layout>
+   );
+}
+ 
+export default Login;
